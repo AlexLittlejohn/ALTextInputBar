f4c71fbdb89fc0acb46cf385be2a1bf67fbc645e
Alex Littlejohn <littlejohn@blazingchilli.com>
2015-04-25 15:33:53 +0200

    Added an example

---
+++ Example/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ALTextInputBar
+//
+//  Created by Alex Littlejohn on 2015/04/24.
+//  Copyright (c) 2015 zero. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
+
---
+++ Example/ViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  ALTextInputBar
+//
+//  Created by Alex Littlejohn on 2015/04/24.
+//  Copyright (c) 2015 zero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let textInputBar = ALTextInputBar()
+    
+    let scrollView = UIScrollView()
+    
+    // The magic sauce
+    // This is how we attach the input bar to the keyboard
+    override var inputAccessoryView: UIView? {
+        get {
+            return textInputBar
+        }
+    }
+    
+    // Another ingredient in the magic sauce
+    override func canBecomeFirstResponder() -> Bool {
+        return true
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureScrollView()
+        configureInputBar()
+    }
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        scrollView.frame = view.bounds
+    }
+
+    func configureScrollView() {
+        view.addSubview(scrollView)
+        
+        let contentView = UIView(frame: CGRectMake(0, 0, view.bounds.size.width, view.bounds.size.height * 2))
+        contentView.backgroundColor = UIColor.groupTableViewBackgroundColor()
+        
+        scrollView.addSubview(contentView)
+        scrollView.contentSize = contentView.bounds.size
+        scrollView.keyboardDismissMode = UIScrollViewKeyboardDismissMode.Interactive
+        scrollView.backgroundColor = UIColor.groupTableViewBackgroundColor()
+    }
+    
+    func configureInputBar() {
+        let leftButton = UIButton(frame: CGRectMake(0, 0, 44, 44))
+        let rightButton = UIButton(frame: CGRectMake(0, 0, 44, 44))
+        
+        leftButton.setImage(UIImage(named: "leftIcon"), forState: UIControlState.Normal)
+        rightButton.setImage(UIImage(named: "rightIcon"), forState: UIControlState.Normal)
+        
+        textInputBar.leftView = leftButton
+        textInputBar.rightView = rightButton
+        
+        textInputBar.backgroundColor = UIColor.whiteColor()
+    }
+
+}
+
