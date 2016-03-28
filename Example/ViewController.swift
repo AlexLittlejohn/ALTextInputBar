acdef6478dc376fe1aee46f470bc964051c4c66e
Alex Littlejohn <alexlittlejohn@me.com>
2016-03-28 22:14:54 +0200

    Updated example with new selector syntax

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -33,9 +33,9 @@ class ViewController: UIViewController {
         configureScrollView()
         configureInputBar()
         
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardFrameChanged:", name: ALKeyboardFrameDidChangeNotification, object: nil)
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(keyboardFrameChanged(_:)), name: ALKeyboardFrameDidChangeNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIKeyboardWillHideNotification, object: nil)
     }
 
     override func viewWillLayoutSubviews() {
