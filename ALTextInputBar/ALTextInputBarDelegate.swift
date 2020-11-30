5a0f76f5c678b8fa83f39ead6b4d8b94fbef2459
L4grange <l4grange@gmail.com>
2020-11-30 14:05:15 +0200

    Migrated to Swift 5.

--- ALTextInputBar/ALTextInputBarDelegate.swift
+++ ALTextInputBar/ALTextInputBarDelegate.swift
@@ -12,13 +12,13 @@ import UIKit
 public protocol ALTextInputBarDelegate: NSObjectProtocol {
     @objc optional func textViewShouldBeginEditing(textView: ALTextView) -> Bool
     @objc optional func textViewShouldEndEditing(textView: ALTextView) -> Bool
-    
+
     @objc optional func textViewDidBeginEditing(textView: ALTextView)
     @objc optional func textViewDidEndEditing(textView: ALTextView)
-    
+
     @objc optional func textViewDidChange(textView: ALTextView)
     @objc optional func textViewDidChangeSelection(textView: ALTextView)
-    
+
     @objc optional func textView(textView: ALTextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool
     
     @objc optional func inputBarDidChangeHeight(height: CGFloat)
